f2e1a1b80f4c46e3f4c43ac5a6bdbcd5ff45d27f
zhangfan490 <1026543100@qq.com>
2020-11-27 19:15:09 +0800

    python实战1_发工资_save_money.py

--- python_pratice/save_money/send.py
+++
@@ -1,7 +0,0 @@
-# 2定义发工资模块
-import money
-
-
-def send_money():
-    money.save_money = 2000
-    print('发工资啦')
--- python_pratice/save_money/start.py
+++
@@ -1,9 +0,0 @@
-# 4启动方法
-
-
-from send_money import send_money
-from slect_money import slect_money
-
-if _name_ == '_main_':
-    send_money()
-    slect_money()
